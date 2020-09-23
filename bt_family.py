4aa828b28fae3b42761b4d7aa469aaf07307aafd
100yuan <47682768+100yuan@users.noreply.github.com>
2020-09-23 19:47:42 +0800

    Update v1.01
    remove Threading method.

--- bt_family.py
+++ bt_family.py
@@ -1,36 +1,18 @@
-# VERSION: 1.00
-# AUTHORS: xuziqiang (thelive1@qq.com)
+# VERSION: 1.01
+# AUTHORS: xuziqiang (158856471@qq.com)
 
 # LICENSING INFORMATION
 
-from requests import get
+from urllib.request import Request, urlopen
 from re import findall, search, S
-from threading import Thread
-from helpers import download_file
 from novaprinter import prettyPrinter
 
-
-#############OutSiteMethod#############
-def __help():
-    return '''Options: （插件高级用法,在搜索栏输入以下标签.）
- ....... --help	显示帮助.
- ...... -n*     选择频道:
- .......... -n1	最新电影
- .......... -n2	720高清
- .......... -n3	1080p高清
- .......... -n4	3D电影下载
- .......... -n5	蓝光电影
- .......... -n9	福利影片
- ......... -n10	电视剧'''
-
-
-def scan_print(msg=__help()):
-    msg = msg.split('\n', -1)
-    for i in range(0, len(msg)):
-        print(f'-1|{i}   {msg[i]}|-1|-1|-1|-1')
-
-
-#############!OutSiteMethod############
+try:
+    # python3
+    from urllib.parse import quote
+except ImportError:
+    # python2
+    from urllib import quote
 
 
 class bt_family(object):
@@ -47,20 +29,36 @@ class bt_family(object):
     name = 'BT之家'
     supported_categories = {'all': '0', 'movies': '6', 'tv': '4'}
 
-    def __init__(self):
-        self.__header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'}
-        self.__page_list = []
+    __header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'}
+    __page_list = []
 
-    def __get_url(self, url):
+    __help = '''Options: （插件高级用法,在搜索栏输入以下标签.）
+     ....... --help	显示帮助.
+     ...... -n*     选择频道:
+     .......... -n1	最新电影
+     .......... -n2	720高清
+     .......... -n3	1080p高清
+     .......... -n4	3D电影下载
+     .......... -n5	蓝光电影
+     .......... -n9	福利影片
+     ......... -n10	电视剧'''
+
+    def __scan_print(self):
+        msg = self.__help.split('\n', -1)
+        for i in range(0, len(msg)):
+            print(f'-1|{i}   {msg[i]}|-1|-1|-1|-1')
+
+    def __urlGet(self, url):
+        link = Request(url, headers=self.__header)
         try:
-            response = get(url, headers=self.__header, timeout=0.6)
+            response = urlopen(link, timeout=0.6).read()
         except:
-            return self.__get_url(url)
-        return response.text
+            return self.__urlGet(url)
+        return response.decode('utf-8')
 
     def __select(self, what):
         if what == '--help':
-            scan_print()
+            self.__scan_print()
             return 'end'
         elif what[:2] == '-n':
             return '/forum-index-fid-' + what[2:].strip()
@@ -68,22 +66,24 @@ class bt_family(object):
             return '/search-index-fid-0-orderby-timedesc-daterage-0-keyword-' + what
 
     def __desc_list(self, url):
-        bt_list = self.__get_url(url)
+        bt_list = self.__urlGet(url)
         bt_list = findall(r'<a target="[^"]+" href="([^"]+)" class="[^"]+">(.*?)</a>', bt_list)
-        self.__page_list += bt_list
+        for i in bt_list:
+            self.__get_dic_lis(i)
 
     def __url_msg(self, what):
         path = self.__select(what)
         if path == 'end':
-            return 'end'
+            return
         urls = []
         for currPage in range(1, 41):
             urls.append(self.url + path + f'-page-{currPage}.htm')
-        self.__thread(urls, self.__desc_list)
+        for i in urls:
+            self.__desc_list(i)
 
     def __get_dic_lis(self, desc_link):
         url = desc_link[0]
-        response = self.__get_url(url)
+        response = self.__urlGet(url)
         msgs = findall(
             r'<td colspan="6">.*?<a href="([^"]+)".*?/>([^>]+)</a>.*?<td> *(\d+) *次</td>\s*<td class="grey">(.*?)</td>',
             response, S)
@@ -93,33 +93,16 @@ class bt_family(object):
                 size = search(r'\d+\.?\d* ?(?:G|M|K)(?=B?]?)', desc_link[1])[0] + 'B'
             except:
                 size = '-1'
-            link = link.replace('dialog', 'download').replace('-ajax-1', '')
+            link = quote(link.replace('dialog', 'download').replace('-ajax-1', ''), safe='/:')
             dic = {'name': name, 'seeds': '-1', 'leech': lench, 'size': size, 'link': link, 'desc_link': url,
                    'engine_url': self.url}
             prettyPrinter(dic)
 
-    def __thread(self, argv_lis, target):
-        tables = []
-        for i in argv_lis:
-            table = Thread(target=target, args=[i])
-            tables.append(table)
-            table.start()
-        for table in tables:
-            table.join()
-
     def download_torrent(self, info):
+        from helpers import download_file
         print(download_file(info))
 
     # DO NOT CHANGE the name and parameters of this function
     # This function will be the one called by nova2.py
     def search(self, what, cat='all'):
-        statu = self.__url_msg(what)
-        if statu != 'end':
-            for i in self.__page_list:
-                Thread(target=self.__get_dic_lis, args=[i]).start()
-
-'''
-if __name__ == '__main__':
-    c = bt_family()
-    c.search('-n1')
-'''
+        self.__url_msg(what)
